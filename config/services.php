d426d1e77dd21e482aaea6b43d997aa83021e1ce
Julien Bohy <julien.bohy@fastback.be>
2026-07-10 14:42:16 +0200

    feat: squelette du bundle (AbstractBundle + services.php)

---
+++ config/services.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $container): void {
+    // Les services (GitRepository, GitDataCollector) sont ajoutés aux étapes suivantes.
+};
